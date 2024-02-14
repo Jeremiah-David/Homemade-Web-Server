0fa2b7fb4c6dfaeea2b0d672f93ed522a84eab40
Jeremiah J David <Dancesfire@gmail.com>
2024-02-14 11:14:12 -0800

    Fix require typo and add GET request handling

--- webserver.js
+++ webserver.js
@@ -1,12 +1,35 @@
-const net = reuired('net')
+const net = require('net')
+const fs = require('fs')
 
 const server = net.createServer((socket) => {
   socket.on('data', (buffer) => {
     const requestString = buffer.toString('utf-8')
-    console.log(requestString)
+    const request = parseRequest(requestString)
+    console.log(request.method, request.path, request.protocol, "request  received  from  client ")    
+    
+    if(request.method === 'GET' ){
+      if (fs.existsSync(`.${request.path}`)) {
+        socket.write('HTTP/1.1 200 OK\n\nHello World')
+      } else {
+        socket.write('HTTP/1.1 404 Not Found\n\n')
+      }
+
+    }
+
+
   })
 })
 
+
+const parseRequest = (requestString) => {
+  const [method, path, protocol] = requestString.split(' ')
+  return {
+    method,
+    path,
+    protocol
+  
+  }
+}
 server.listen(8080, () => {
   console.log('Server started')
 })
